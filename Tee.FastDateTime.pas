62ad1306f26fc0c876a07b87d22699cbb86aa7b5
David Berneda <david@steema.com>
2016-07-03 18:44:24 +0200

    Reduced code and improved speed thanks to David Heffernan (https://plus.google.com/u/0/103246155735524926641) using System.Math DivMod instead of the one in DateUtils

--- Tee.FastDateTime.pas
+++ Tee.FastDateTime.pas
@@ -2,6 +2,11 @@
 // David Berneda @davidberneda July-2016
 unit Tee.FastDateTime;
 
+// Important for speed:
+// When not debugging, always compile this unit with Optimization ON,
+// Inlining ON, and Range-checking, Overflow-checking, Assertions OFF
+// {$O+,C-,R-,Q-}
+
 interface
 
 uses
@@ -15,12 +20,15 @@ type
       D4 = D1 * 4 + 1;
       D100 = D4 * 25 - 1;
       D400 = D100 * 4 + 1;
+
+    class function CalcLeap(T: Integer; out D:Word): Boolean; static;
+    class function DayMonth(const T:Integer; out M:Byte): Word; overload; static;
   public
     class function DayOf(const DateTime: TTimeStamp): Word; overload; static;
     class function DayOf(const DateTime: TDateTime): Word; overload; inline; static;
 
-    class function MonthOf(const DateTime: TTimeStamp): Word; overload; static;
-    class function MonthOf(const DateTime: TDateTime): Word; overload; inline; static;
+    class function MonthOf(const DateTime: TTimeStamp): Byte; overload; static;
+    class function MonthOf(const DateTime: TDateTime): Byte; overload; inline; static;
 
     class function YearOf(const DateTime: TTimeStamp): Word; overload; static;
     class function YearOf(const DateTime: TDateTime): Word; overload; inline; static;
@@ -28,15 +36,8 @@ type
 
 implementation
 
-const
-  MonthDays: TDayTable = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-  LeapMonthDays: TDayTable = (31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-
-procedure DivMod(Dividend: Integer; Divisor: Word; out Result, Remainder: Word); inline;
-begin
-  Result    := Dividend div Divisor;
-  Remainder := Dividend mod Divisor;
-end;
+uses
+  System.Math;
 
 // Optimized from DateUtils.pas
 class function TFastDateTime.YearOf(const DateTime: TTimeStamp): Word;
@@ -81,142 +82,101 @@ begin
   end;
 end;
 
-class function TFastDateTime.MonthOf(const DateTime: TTimeStamp): Word;
-var
-  Y, D, I: Word;
-  T: Integer;
+class function TFastDateTime.CalcLeap(T: Integer; out D:Word): Boolean;
+var I,
+    Y : Word;
 begin
-  T := DateTime.Date;
+  Dec(T);
 
-  if T <= 0 then
-     result := 0
+  Y := 1;
+
+  while T >= D400 do
+  begin
+    Dec(T, D400);
+    Inc(Y, 400);
+  end;
+
+  DivMod(T, D100, I, D);
+
+  if I = 4 then
+  begin
+    Dec(I);
+    Inc(D, D100);
+  end;
+
+  Inc(Y, I * 100);
+  DivMod(D, D4, I, D);
+  Inc(Y, I * 4);
+  DivMod(D, D1, I, D);
+
+  if I = 4 then
+  begin
+    Dec(I);
+    Inc(D, D1);
+  end;
+
+  Inc(Y, I);
+
+  result:=IsLeapYear(Y);
+end;
+
+class function TFastDateTime.DayMonth(const T: Integer; out M:Byte): Word;
+type
+  TDayTable = array[1..12] of Byte;
+
+const
+  MonthDays: TDayTable = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+  LeapMonthDays: TDayTable = (31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+
+var I: Byte;
+begin
+  M:=1;
+
+  if CalcLeap(T,result) then
+  begin
+    while True do
+    begin
+      I := LeapMonthDays[M];
+      if result < I then Break;
+      Dec(result, I);
+      Inc(M);
+    end;
+  end
   else
   begin
-    Dec(T);
-
-    Y := 1;
-
-    while T >= D400 do
+    while True do
     begin
-      Dec(T, D400);
-      Inc(Y, 400);
+      I := MonthDays[M];
+      if result < I then Break;
+      Dec(result, I);
+      Inc(M);
     end;
-
-    DivMod(T, D100, I, D);
-
-    if I = 4 then
-    begin
-      Dec(I);
-      Inc(D, D100);
-    end;
-
-    Inc(Y, I * 100);
-    DivMod(D, D4, I, D);
-    Inc(Y, I * 4);
-    DivMod(D, D1, I, D);
-
-    if I = 4 then
-    begin
-      Dec(I);
-      Inc(D, D1);
-    end;
-
-    Inc(Y, I);
-
-    result := 1;
-
-    if IsLeapYear(Y) then
-    begin
-      while True do
-      begin
-        I := LeapMonthDays[result];
-        if D < I then Break;
-        Dec(D, I);
-        Inc(result);
-      end;
-    end
-    else
-    begin
-      while True do
-      begin
-        I := MonthDays[result];
-        if D < I then Break;
-        Dec(D, I);
-        Inc(result);
-      end;
-    end;
-
   end;
+
+  Inc(result);
+end;
+
+class function TFastDateTime.MonthOf(const DateTime: TTimeStamp): Byte;
+var T: Integer;
+begin
+  T:=DateTime.Date;
+
+  if T<=0 then
+     result:=0
+  else
+     DayMonth(T,result);
 end;
 
 class function TFastDateTime.DayOf(const DateTime: TTimeStamp): Word;
-var
-  Y, M, I: Word;
-  T: Integer;
+var T: Integer;
+    M: Byte;
 begin
-  T := DateTime.Date;
+  T:=DateTime.Date;
 
-  if T <= 0 then
-     result := 0
+  if T<=0 then
+     result:=0
   else
-  begin
-    Dec(T);
-
-    Y := 1;
-
-    while T >= D400 do
-    begin
-      Dec(T, D400);
-      Inc(Y, 400);
-    end;
-
-    DivMod(T, D100, I, result);
-
-    if I = 4 then
-    begin
-      Dec(I);
-      Inc(result, D100);
-    end;
-
-    Inc(Y, I * 100);
-    DivMod(result, D4, I, result);
-
-    Inc(Y, I * 4);
-    DivMod(result, D1, I, result);
-
-    if I = 4 then
-    begin
-      Dec(I);
-      Inc(result, D1);
-    end;
-
-    Inc(Y, I);
-
-    M := 1;
-
-    if IsLeapYear(Y) then
-    begin
-      while True do
-      begin
-        I := LeapMonthDays[M];
-        if result < I then Break;
-        Dec(result, I);
-        Inc(M);
-      end;
-    end
-    else
-    begin
-      while True do
-      begin
-        I := MonthDays[M];
-        if result < I then Break;
-        Dec(result, I);
-        Inc(M);
-      end;
-    end;
-
-    Inc(result);
-  end;
+     result:=DayMonth(T,M);
 end;
 
 class function TFastDateTime.DayOf(const DateTime: TDateTime): Word;
@@ -224,7 +184,7 @@ begin
   result:=DayOf(DateTimeToTimeStamp(DateTime));
 end;
 
-class function TFastDateTime.MonthOf(const DateTime: TDateTime): Word;
+class function TFastDateTime.MonthOf(const DateTime: TDateTime): Byte;
 begin
   result:=MonthOf(DateTimeToTimeStamp(DateTime));
 end;
